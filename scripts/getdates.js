593cf80b129d2ac58b28b15aded77bacdb48476f
lisbet-dokcom <lissbetjb13@gmail.com>
2024-06-21 12:26:44 +0100

    comiit

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,23 +1,6 @@
-// select the DOM elements for output
-
-const short = document.querySelector("#short");
-
-// use the date object
-const today = new Date();
-
-short.innerHTML = `Short: <span class="highlight">${new Intl.DateTimeFormat(
-    "en-US",
-    {
-        dateStyle: "short"
-    }
-).format(today)}</span>`;
-
-
-// Get the current year
 const currentYear = new Date().getFullYear();
 document.getElementById("currentyear").innerHTML = currentYear;
 
-// Get the last modified date
 const lastModified = document.lastModified;
-document.getElementById("lastModified").innerHTML = `Last Modified: ${lastModified}`;
+document.getElementById("lastModified").textContent = `Last Modified: ${lastModified}`;
 
