e5be16a9caf80b7d998fe4d9951ff0d8bc43bed6
lisbet-dokcom <lissbetjb13@gmail.com>
2024-06-21 13:35:05 +0100

    comit

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -4,5 +4,6 @@ document.getElementById("currentyear").innerHTML = currentYear;
 const lastModified = document.lastModified;
 document.getElementById("lastModified").textContent = `Last Modified: ${lastModified}`;
 
-const name = document.name;
 document.getElementById('name').innerHTML = 'Elizabeth Okon';
+
+
