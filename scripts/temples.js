3c16180710531108bdbe4521cde3d2065e1f4484
lisbet-dokcom <lissbetjb13@gmail.com>
2024-06-28 02:07:49 +0100

    comit

--- scripts/temples.js
+++ scripts/temples.js
@@ -3,3 +3,13 @@ document.getElementById("currentyear").innerHTML = currentYear;
 
 const lastModified = document.lastModified;
 document.getElementById("lastModified").textContent = `Last Modified: ${lastModified}`;
+
+const hazButton = document.querySelector('#menu');
+const nav = document.querySelector('.nav');
+
+hazButton.addEventListener('click', () => {
+    nav.classList.toggle('open');
+    hazButton.classList.toggle('open');
+});
+
+
