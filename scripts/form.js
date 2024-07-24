c46b98c0eba2d3d7ed28b599b3a42a14f2b2799f
lisbet-dokcom <lissbetjb13@gmail.com>
2024-07-24 14:21:27 +0100

    stuck

--- scripts/form.js
+++ scripts/form.js
@@ -7,29 +7,30 @@ document.getElementById("lastModified").textContent = `Last Modified: ${lastModi
 const optional = document.optional;
 document.getElementById("optional").textContent = `(Optional)`;
 
+
 const products = [
     {
-        id: fc - 1888,
+        id: "fc - 1888",
         name: "flux capacitor",
         averagerating: 4.5
     },
     {
-        id: fc - 2050,
+        id: "fc - 2050",
         name: "power laces",
         averagerating: 4.7
     },
     {
-        id: fs - 1987,
+        id: "fs - 1987",
         name: "time circuits",
         averagerating: 3.5
     },
     {
-        id: ac - 2000,
+        id: "ac - 2000",
         name: "low voltage reactor",
         averagerating: 3.9
     },
     {
-        id: jj - 1969,
+        id: "jj - 1969",
         name: "warp equalizer",
         averagerating: 5.0
     }
@@ -46,4 +47,17 @@ function createProductList() {
         productSelect?.appendChild(optElement);
     });
 }
+
+function reviewCountList() {
+    let reviewCount = localStorage.getItem('reviewCount') || 0;
+    reviewCount++;
+    localStorage.setItem('reviewCount', reviewCount);
+    document.querySelector("#reviewCounter").innerHTML = reviewCount;
+
+}
+
 createProductList();
+reviewCountList();
+
+
+
